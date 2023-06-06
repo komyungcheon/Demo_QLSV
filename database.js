e5227474b725a87dec20240015811861d5b2ad40
komyungcheon <shy06101997@gamil.com>
2023-06-06 11:36:34 +0700

    update

---
+++ database.js
@@ -0,0 +1,28 @@
+
+
+// create database homestay;
+// use homestay;
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123';
+// create table Homestay(
+//     id int auto_increment primary key,
+//     name varchar(255),
+//     city varchar(255),
+//     bedrooms int,
+//     price int,
+//     toilet int,
+//     describes varchar(255)
+// );
+//
+//
+// insert into Homestay (name,city,bedrooms,price,toilet,describes) values
+// ('tam dao','vinh phuc',1,2000000,1,'mo ta 1'),
+//     ('ba vi','ha noi',2,2500000,1,'mo ta 2'),
+//     ('dai nai','vinh phuc',1,300000,1,'mo ta 3'),
+//     ('trang an','ninh binh',4,1800000,1,'mo ta 4'),
+//     ('vinh yen','vinh phuc',2,1500000,1,'mo ta 5'),
+//     ('phu quoc','kien giang',3,5000000,1,'mo ta 6');
+
+
+
+
+
